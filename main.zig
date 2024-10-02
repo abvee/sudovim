4b0ad2fa9f69e2476997182bae5a93ac39ebe5c0
abvee <icaliberdev@gmail.com>
2024-10-02 15:19:36 +0530

    execvpe arguments made cleaner

--- main.zig
+++ main.zig
@@ -1,24 +1,25 @@
 const std = @import("std");
-
 const posix = std.posix;
 
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-	// @compileLog(@TypeOf(stdout));
-	const editor = posix.getenvZ("EDITOR") orelse blk:{
-		// we don't want to break the program if stdout is closed
+
+	const editor = posix.getenvZ("EDITOR") orelse blk: {
+		// we don't want to break the program is stdout is closed
 		stdout.print("$EDITOR not found, using vim\n", .{}) catch {};
 		break :blk "vim";
 	};
 
 	const pid = try posix.fork();
-	if (pid == 0) {
-		const x: ?[*:0]const u8 = null;
-		posix.execvpeZ(editor, @ptrCast(&x), @ptrCast(&x)) catch {};
-	}
+	// NOTE: don't use catch |err| here, posix.execvpeZ returns an error set
+	// and as of zig 13, you cannot use capture groups with them. `try` is
+	// also a form of catch |err|.
+	if (pid == 0)
+		posix.execvpeZ(editor, &.{null}, &.{null}) catch
+			std.debug.print("Something went wrong with calling exec.\n", .{});
 
-	stdout.print("pid: {}\n", .{pid}) catch {};
+	stdout.print("editor pid: {}\n", .{pid}) catch {};
 	stdout.print("editor {s}\n", .{editor}) catch {};
 	_ = posix.waitpid(pid, 0);
 }
