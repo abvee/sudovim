618211113c544659f80af9e1e7604494ccb4fc4c
abvee <icaliberdev@gmail.com>
2024-11-29 11:20:57 +0530

    process spawning tests

--- main.zig
+++ main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
+const process = std.process;
 
 pub fn main() !void {
+
 	std.debug.print("Hello world\n", .{});
+}
+
+test "zig process spawning" {
+
+	// allocator
+	var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+	defer arena.deinit();
+	const allocator = arena.allocator();
+
+	// init a process
+	var child: process.Child = process.Child.init(
+		&.{"bash", "./tests/spawn-test.sh"},
+		allocator,
+	);
+	std.debug.print("{any}\n", .{child});
+
+	// spawn it
+	try child.spawn();
+	std.debug.print("After spawning\n", .{});
+
+	// Wait for it to finish
+	_ = try child.wait();
 }
