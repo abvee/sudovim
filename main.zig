3f73e42d07f73157481f1e037bd97bf859b38006
abvee <icaliberdev@gmail.com>
2024-11-29 11:28:58 +0530

    Spawn root process

--- main.zig
+++ main.zig
@@ -2,8 +2,22 @@ const std = @import("std");
 const process = std.process;
 
 pub fn main() !void {
+	// allocator
+	var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+	defer arena.deinit();
+	const allocator = arena.allocator();
 
-	std.debug.print("Hello world\n", .{});
+	// init a process
+	var child: process.Child = process.Child.init(
+		&.{"/bin/doas", "vim"},
+		allocator,
+	);
+
+	// spawn process
+	try child.spawn();
+
+	// wait for it to finish
+	_ = try child.wait();
 }
 
 test "zig process spawning" {
